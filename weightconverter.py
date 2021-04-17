62358e689933b22b09dfed85f0003132a751bdf5
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-17 18:14:03 -0400

    Delete weightconverter.py

--- weightconverter.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name? ")
-weight_lbs = input('How much do you weigh in lbs? ')
-weight_kgs = int(weight_lbs) * 0.45
-print('Hello ' + name + "!")
-print('You weigh ' + str(weight_kgs) + ' kgs.')
