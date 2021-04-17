53213917a0e251a7aff7bf28e0a40a8223b3c585
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-17 19:25:01 -0400

    Update ExerciseRepCalculator.py

--- ExerciseRepCalculator.py
+++ ExerciseRepCalculator.py
@@ -1,6 +1,13 @@
+# needs an "invalid entry" option....
 print("""
 Use this calculator to determine how many total reps you are
-doing of an exercise if you reduce your count by 1 each set""")
-starting_reps = int(input("\nHow many reps to start? Enter a number: "))
-total_no_reps = (1 + starting_reps)*(starting_reps/2)
-print(f"{int(total_no_reps)} total reps")
+doing of an exercise if you reduce your count by 1 each set.
+Enter "quit" to exit.""")
+while True:
+    command = input("\nHow many reps to start? Enter a number: ")
+    if command.lower() == "quit":
+        break
+    else:
+        starting_reps = int(command)
+        total_no_reps = (1 + starting_reps)*(starting_reps/2)
+        print(f"{int(total_no_reps)} total reps")
